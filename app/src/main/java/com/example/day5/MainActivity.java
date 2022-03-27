3cfe7295d43209e22ce08d23d98548af88e4fa14
GIO <programology.gio@gmail.com>
2022-03-28 00:38:35 +0400

    Initial commit

---
+++ app/src/main/java/com/example/day5/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.day5;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = findViewById(R.id.imageView2);
+        btn = findViewById(R.id.butn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 6);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+        if (requestCode==6) {
+            Bitmap b;
+            b = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(b);
+        }
+    }
+}
